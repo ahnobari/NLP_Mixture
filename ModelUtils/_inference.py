603d5157c1a51811abedfcdb323b5b2204f71397
ahnobari <ahnobari@mit.edu>
2024-11-19 16:42:35 -0500

    Batch run support added but its  slower anyways

--- ModelUtils/_inference.py
+++ ModelUtils/_inference.py
@@ -10,3 +10,14 @@ def run_model(inputs, model, tokenizer, **kwargs):
         outputs.append(output)
     
     return outputs
+
+def run_model_batch(inputs, attention_masks, model, tokenizer, batch_size=3, **kwargs):
+    outputs = []
+    for i in trange(0, len(inputs), batch_size):
+        input_tensor = torch.tensor(inputs[i:i+batch_size]).to(model.device)
+        attention_mask = torch.tensor(attention_masks[i:i+batch_size]).to(model.device)
+        output = model.generate(input_ids = input_tensor, attention_mask=attention_mask, pad_token_id = tokenizer.eos_token_id, **kwargs)
+        output = tokenizer.batch_decode(output, skip_special_tokens=True)
+        outputs += output
+    return outputs
+    
--- ModelUtils/_processing.py
+++ ModelUtils/_processing.py
@@ -1,4 +1,5 @@
 from tqdm.auto import tqdm, trange
+import torch
 
 def tokenize_chat(inputs, tokenizer):
     for i in trange(len(inputs)):
@@ -8,6 +9,16 @@ def tokenize_chat(inputs, tokenizer):
             tokenized_inputs += [tokenizer.apply_chat_template(inputs[i],add_generation_prompt=True)]
     
     return tokenized_inputs
+
+def tokenize_chat_batch(inputs, tokenizer):
+    tokenizer.pad_token = tokenizer.eos_token
+    tokens = tokenizer.apply_chat_template(inputs, padding=True, add_generation_prompt=True, return_tensors="pt")
+    n_padding = torch.sum((tokens == tokenizer.pad_token_id)[:,:-1] * (tokens == tokenizer.pad_token_id)[:,1:],dim=1)
+    n_padding[n_padding != 0] += 1
+    attention_mask = 1-torch.cumsum(torch.nn.functional.one_hot(tokens.shape[1]-n_padding, num_classes=tokens.shape[1]+1),dim=1)
+    attention_mask = attention_mask[:,:-1]
+    
+    return tokens, attention_mask
 
 def tokenize_plain(inputs, tokenizer):
     for i in trange(len(inputs)):
