ef9ef89c73b7852583cc38ef147192369d31da0a
Your Name <mream79123@gmail.com>
2024-11-20 22:28:07 +0000

    Added MATH benchmark

--- DataUtils/MATH.py
+++ DataUtils/MATH.py
@@ -1,1 +1,1 @@
-from ._MATH import get_test_data_chat_template, get_test_data_plain, extract_answers
+from ._MATH import get_test_data_chat_template, get_test_data_plain, extract_answers, get_input_kwargs
--- DataUtils/_MATH.py
+++ DataUtils/_MATH.py
@@ -25,9 +25,10 @@ def get_test_data_chat_template(jsonl_path='./MATH/MATH_test.jsonl'):
         response = data[i]['output']
         
         # extract what is inside \boxed{}
-        response = response.split("\\boxed{")
-        response = response[1].split("}")
-        response = response[0]
+        #response = response.split("\\boxed{")
+        #response = response[1][:-1] #I'm assuming } is the last char, we need to fix this later.
+        #response = response[0]
+        response = extract_answer(response)
         Answers.append(response)
 
     return inputs, Answers
@@ -45,12 +46,44 @@ def get_test_data_plain(jsonl_path='./MATH/MATH_test.jsonl'):
         inputs.append(prompt)
         
         response = data[i]['output']
-        response = response.split("\\boxed{")
-        response = response[1].split("}")
-        response = response[0]
+        #response = response.split("\\boxed{")
+        #response = response[1] #I'm assuming } is the last char, we need to fix this later.
+        #response = response[0]
+        response = extract_answer(response)
         Answers.append(response)
 
     return inputs, Answers
 
 def extract_answers(responses):
-    return [response.split("\\boxed{")[-1].split("}")[0] for response in responses]
+    return [extract_answer(response) for response in responses]
+
+
+def get_input_kwargs(tokenizer, choices = ["A", "B", "C", "D"]):
+    
+    for i in range(len(choices)):
+        choices[i] = problem_prompt[-1].format(answer=choices[i])
+        
+    force_words_ids = [tokenizer(choices, add_special_tokens=False).input_ids]
+    max_new_tokens = 500
+    num_beams = 2
+    do_sample = False
+    top_p = 0.9
+    
+    return {"max_new_tokens": max_new_tokens, "num_beams": num_beams, "do_sample": do_sample, "top_p": top_p}
+
+def find_first_unopened_closing_brace(a):
+    x = 0
+    for i, char in enumerate(a):
+        if char == '{':
+            x += 1 
+        elif char == '}':
+            x -= 1
+        if x == -1:
+            return a[:i]
+        
+    return a
+
+def extract_answer(response):
+    a = response.split("\\boxed{")[-1]
+    a = find_first_unopened_closing_brace(a)
+    return a
