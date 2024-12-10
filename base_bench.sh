0e6be09fe973448bdfe38bf65fb54c75cb29a4b5
KavehAlim <mream79123@gmail.com>
2024-12-10 10:46:32 -0500

    Updated Readme

--- base_bench.sh
+++ base_bench.sh
@@ -1,33 +1,6 @@
 #!/bin/bash
 
-models=('flitered_base/average_merging'
-        'flitered_base/mask_merging'
-        'flitered_base/task_arithmetic'
-        'flitered_base/ties_merging'
-        'flitered_base/widen_merging'
-        'flitered_instruct/average_merging'
-        'flitered_instruct/mask_merging'
-        'flitered_instruct/task_arithmetic'
-        'flitered_instruct/ties_merging'
-        'flitered_instruct/widen_merging'
-        'ActiveM_instruct/ActiveM_0.1_1.0'
-        'ActiveM_instruct/ActiveM_0.01_1.0'
-        'ActiveM_instruct/ActiveM_0.05_1.0'
-        'ActiveM_instruct/ActiveM_0.1_2.0'
-        'ActiveM_instruct/ActiveM_0.01_2.0'
-        'ActiveM_instruct/ActiveM_0.05_2.0'
-        'ActiveM_instruct/ActiveM_0.1_5.0'
-        'ActiveM_instruct/ActiveM_0.01_5.0'
-        'ActiveM_instruct/ActiveM_0.05_5.0'
-        'ActiveM_base/ActiveM_0.1_1.0'
-        'ActiveM_base/ActiveM_0.01_1.0'
-        'ActiveM_base/ActiveM_0.05_1.0'
-        'ActiveM_base/ActiveM_0.1_2.0'
-        'ActiveM_base/ActiveM_0.01_2.0'
-        'ActiveM_base/ActiveM_0.05_2.0'
-        'ActiveM_base/ActiveM_0.1_5.0'
-        'ActiveM_base/ActiveM_0.01_5.0'
-        'ActiveM_base/ActiveM_0.05_5.0')
+models=('merged_models/task_arithmetic')
 
 for model in "${models[@]}"
 do
